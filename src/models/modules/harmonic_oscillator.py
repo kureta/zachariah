85f8629b4d1af82dd8c84dd415c6182c76804fba
kureta <skureta@gmail.com>
2022-01-01 22:11:19 +0300

    Cleaning up harmonic_oscillator.py

--- src/models/modules/harmonic_oscillator.py
+++ src/models/modules/harmonic_oscillator.py
@@ -1,13 +1,12 @@
-from typing import Tuple
-
 import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F  # noqa
+from einops import rearrange
 
 
 class OscillatorBank(nn.Module):
-    def __init__(self, n_harmonics: int = 60, sample_rate: int = 16000, hop_length: int = 256):
+    def __init__(self, n_harmonics: int = 60, sample_rate: int = 16000, hop_length: int = 64):
         super().__init__()
 
         self.n_harmonics = n_harmonics
@@ -16,44 +15,62 @@ class OscillatorBank(nn.Module):
 
         self.harmonics = torch.arange(1, self.n_harmonics + 1, step=1)
 
-    def prepare_harmonics(self, f0: torch.Tensor, harm_amps: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
-        # Cut above Nyquist and normalize
-        # Hz (cycles per second)
-        harmonics = torch.einsum("ijk,k->ijk", f0, self.harmonics)
-        # zero out above nyquist
-        mask = harmonics > self.sample_rate // 2
-        harm_amps = harm_amps.masked_fill(mask, 0.0)
-        # normalize distribution
-        # harm_amps /= harm_amps.sum(-1, keepdim=True)
-        harmonics *= 2 * np.pi  # radians per second
-        harmonics /= self.sample_rate  # radians per sample
-        harmonics = self.rescale(harmonics)
-        return harmonics, harm_amps
+    def get_harmonic_frequencies(self, f0: torch.Tensor) -> torch.Tensor:
+        # f0.shape = [batch, time, ch]
+        # Calculate harmonic frequencies in cycles per seconds (Hz).
+        harmonic_frequencies = torch.einsum("ijk,k->ijk", f0, self.harmonics)
 
-    @staticmethod
-    def generate_phases(harmonics: torch.Tensor) -> torch.Tensor:
-        phases = torch.cumsum(harmonics, dim=1)
+        harmonic_frequencies *= 2 * np.pi  # radians per second
+        harmonic_frequencies /= self.sample_rate  # radians per sample
+
+        # harmonic_frequencies.shape = [batch, time, n_harmonics]
+        return harmonic_frequencies
+
+    def get_phases(self, harmonic_frequencies: torch.Tensor) -> torch.Tensor:
+        # harmonic_frequencies.shape = [batch, time, n_harmonics]
+        harmonic_frequencies = self.rescale(harmonic_frequencies)
+        phases = torch.cumsum(harmonic_frequencies, dim=1)
         phases %= 2 * np.pi
+        # phases.shape = [batch, time, n_harmonics]
         return phases
 
-    def generate_signal(self, harm_amps: torch.Tensor, loudness: torch.Tensor, phases: torch.Tensor) -> torch.Tensor:
-        loudness = self.rescale(loudness)
-        harm_amps = self.rescale(harm_amps)
-        signal = loudness * harm_amps * torch.sin(phases)
+    def get_signal(
+        self, harmonic_distribution: torch.Tensor, amplitude: torch.Tensor, phases: torch.Tensor
+    ) -> torch.Tensor:
+        # harmonic_distribution.shape = [batch, time, n_harmonics]
+        # amplitude.shape = [batch, time, ch]
+        # phases.shape = [batch, time, n_harmonics]
+        amplitude = self.rescale(amplitude)
+        harmonic_distribution = self.rescale(harmonic_distribution)
+        signal = amplitude * harmonic_distribution * torch.sin(phases)
         signal = torch.sum(signal, dim=2)
+        # signal.shape = [batch, time, ch]
         return signal
 
     def rescale(self, x: torch.Tensor) -> torch.Tensor:
-        return F.interpolate(
-            x.permute(0, 2, 1),
-            scale_factor=float(self.hop_size),
-            mode="linear",
+        # x.shape = [batch, time, ch]
+        x = F.interpolate(
+            rearrange(x, "b t c -> b c 1 t"),
+            scale_factor=(1, float(self.hop_size)),
+            mode="bicubic",
             align_corners=True,
-        ).permute(0, 2, 1)
+        )
 
-    def forward(self, f0: torch.Tensor, loudness: torch.Tensor, harm_amps: torch.Tensor) -> torch.Tensor:
-        harmonics, harm_amps = self.prepare_harmonics(f0, harm_amps)
-        phases = self.generate_phases(harmonics)
-        signal = self.generate_signal(harm_amps, loudness, phases)
+        # output.shape = [batch, time, ch]
+        return rearrange(x, "b c 1 t -> b t c")
 
+    def forward(self, f0: torch.Tensor, amplitude: torch.Tensor, harmonic_distribution: torch.Tensor) -> torch.Tensor:
+        # f0.shape = [batch, time, ch]
+        # amplitude.shape = [batch_time, ch]
+        # harmonic_distribution.shape = [batch, time, n_harmonics]
+        harmonic_frequencies = self.get_harmonic_frequencies(f0)
+        phases = self.get_phases(harmonic_frequencies)
+
+        # Zero out above nyquist
+        mask = harmonic_frequencies > np.pi
+        harmonic_distribution = harmonic_distribution.masked_fill(mask, 0.0)
+
+        signal = self.get_signal(harmonic_distribution, amplitude, phases)
+
+        # signal.shape = [batch, time, channel]
         return signal
